ae57e4634bce41db7653cdbe11af7273b547f082
posix <posix.lee@gmail.com>
2019-10-12 03:30:28 +0900

    1012

--- mergeDeep/test.js
+++ mergeDeep/test.js
@@ -8,7 +8,7 @@ function check(root) {
     var root_params = [];
     var root_forin_params = {};
     var root_expressions = [];
-    var src_name, property_name;
+    var src_name, property_name, root_func_name;
 
     traverse(root);
     
@@ -17,11 +17,10 @@ function check(root) {
     src_name = root_forin_params.src_name;
     property_name = root_forin_params.property_name;
 
+
     for (let expression of root_expressions) {
-        let { type, operator, left, right } = expression;
-        if (type !== 'AssignmentExpression' || operator !== '=') continue;
-        if (right.object.name !== src_name) continue;
-        if (left.property.name !== right.property.name || left.property.name !== property_name) continue;
+        let { type, callee } = expression;
+        if (type !== 'CallExpression' || callee.name !== root_func_name) continue;
 
         return true;
     }
@@ -30,16 +29,21 @@ function check(root) {
 
     function traverse(node) {
         if (!node) { return; }
-        let { type, body, params, expression, left, right } = node;
+        let { type, body, params, expression, left, right, consequent, alternate, id } = node;
 
-        if (['Program', 'FunctionDeclaration', 'BlockStatement', 'ForInStatement', 'ExpressionStatement'].includes(type)) {
+        if (['Program', 'FunctionDeclaration', 'BlockStatement', 'ForInStatement', 'ExpressionStatement', 'IfStatement'].includes(type)) {
             if (type === 'FunctionDeclaration') {
                 root_params = root_params.concat(params.map($=>$.name));
+                root_func_name = id.name;
             } else if (type === 'ExpressionStatement') {
                 root_expressions.push(expression);
             } else if (type === 'ForInStatement') {
                 root_forin_params.src_name = right.name;
                 root_forin_params.property_name = left.declarations[0].id.name;
+            } else if (type === 'IfStatement') {
+                body = [];
+                if (consequent) body.push(consequent);
+                if (alternate) body.push(alternate);
             }
     
             if (body) {
